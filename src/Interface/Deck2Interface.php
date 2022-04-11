122492badb4dea022c7053c4330dd635524595b6
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-04-11 21:15:09 +0100

    DeckWithTwoJokers interface

---
+++ src/Interface/Deck2Interface.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Interface;
+
+interface Deck2Interface
+{
+
+    /**
+     * Get all cards in deck.
+     *
+     * @return array with all cards in deck.
+     */
+    public function getDeck();
+
+    /**
+     * Create deck.
+     *
+     * @return void
+     */
+    public function createDeck();
+
+    /**
+     * Sort deck.
+     *
+     * @return void
+     */
+    public function sortCards();
+
+    /**
+     * Draw a number of cards from the deck.
+     *
+     * @return array with all drawn cards.
+     */
+    public function drawCards(int $number);
+
+        /**
+     * Add two jokers to the deck.
+     *
+     * @return void
+     */
+    public function addJokers();
+}
