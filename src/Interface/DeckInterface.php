a7989f2d2182faeb65ac81e764e4c18079805956
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-04-11 21:13:49 +0100

    Deck interface

---
+++ src/Interface/DeckInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Interface;
+
+interface DeckInterface
+{
+
+    /**
+     * Get all cards in deck.
+     *
+     * @return array with all cards in deck.
+     */
+    public function getDeck();
+
+    /**
+     * Create deck.
+     *
+     * @return void
+     */
+    public function createDeck();
+
+    /**
+     * Sort deck.
+     *
+     * @return void
+     */
+    public function sortCards();
+
+    /**
+     * Draw a number of cards from the deck.
+     *
+     * @return array with all drawn cards.
+     */
+    public function drawCards(int $number);
+}
