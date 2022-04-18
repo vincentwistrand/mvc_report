df6be6673fb0710fa4d8f50b7e133d1f6ff38aec
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-04-18 08:51:45 +0100

    Made properties private and added get methods instead

--- src/Card/Card.php
+++ src/Card/Card.php
@@ -4,14 +4,38 @@ namespace App\Card;
 
 class Card
 {
-    public ?string $colour = "";
-    public ?string $points = "";
-    public ?string $rank = "";
+    private string $colour = "";
+    private string $points = "";
+    private string $rank = "";
 
-    public function __construct(?string $rank = "", ?string $colour = "", ?string $points = "")
+    public function __construct(string $rank = "", string $colour = "", string $points = "")
     {
         $this->colour = $colour;
         $this->rank = $rank;
         $this->points = $points;
+    }
+
+    /**
+    * @return string
+    */
+    public function getColour(): string
+    {
+        return $this->colour;
+    }
+
+    /**
+    * @return string
+    */
+    public function getPoints(): string
+    {
+        return $this->points;
+    }
+
+    /**
+    * @return string
+    */
+    public function getRank(): string
+    {
+        return $this->rank;
     }
 }
