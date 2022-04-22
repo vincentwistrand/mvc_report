faa87fffe098bcda45666201d0f0b6ec58c3d841
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-04-22 16:57:54 +0100

    PHPUnit test case testing Player class

---
+++ tests/Card/PlayerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Card;
+use App\Card\Player;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test cases for class CardTDD.
+ */
+class PlayerTest extends TestCase
+{
+    /**
+     * Construct object and verify that the object is of expected instance.
+     * Use no arguments.
+     */
+    public function testCreate()
+    {
+        $player = new Player("Player");
+        $this->assertInstanceOf("\App\Card\Player", $player);
+    }
+}
