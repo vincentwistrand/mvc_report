dc9114613ccd3c85c6a21f9c90415232ac8026be
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-04-23 17:17:12 +0100

    Made  attribute player_id private and added method getPlayerId() instead

--- src/Card/Player.php
+++ src/Card/Player.php
@@ -4,10 +4,18 @@ namespace App\Card;
 
 class Player extends CardHand
 {
-    public string $player_id;
+    private string $player_id;
 
     public function __construct(string $player_name)
     {
         $this->player_id = $player_name;
+    }
+
+    /**
+    * @return string
+    */
+    public function getPlayerId(): string
+    {
+        return $this->player_id;
     }
 }
