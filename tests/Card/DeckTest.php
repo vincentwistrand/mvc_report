421fd30ac251efa0c7f82d9b8ca287e185fd39b7
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-04-23 18:56:42 +0100

    Test file finished

--- tests/Card/DeckTest.php
+++ tests/Card/DeckTest.php
@@ -28,7 +28,7 @@ class DeckTest extends TestCase
     public function testGetDeck()
     {   
         $deck = new Deck();
-        $deck->createDeck();
+        $this->assertEquals(null, $deck->createDeck());
         $this->assertEquals(52, count($deck->getDeck()));
     }
 
@@ -44,5 +44,54 @@ class DeckTest extends TestCase
         $this->assertEquals(51, count($deck->getDeck()));
         $cards = $deck->drawCards(6);
         $this->assertEquals(45, count($deck->getDeck()));
+    }
+
+    /**
+     * Sort cards using sortCards() and verify that it is sorted.
+     * Use no arguments.
+     */
+    public function testSortCards()
+    {   
+        $deck = new Deck();
+        $deck->createDeck();
+        $deck->sortCards();
+        $this->assertEquals("2", $deck->getDeck()[0]->getRank());
+        $this->assertEquals("3", $deck->getDeck()[1]->getRank());
+        $this->assertEquals("2", $deck->getDeck()[13]->getRank());
+        $this->assertEquals("3", $deck->getDeck()[14]->getRank());
+        $this->assertEquals("2", $deck->getDeck()[26]->getRank());
+        $this->assertEquals("3", $deck->getDeck()[27]->getRank());
+        $this->assertEquals("2", $deck->getDeck()[39]->getRank());
+        $this->assertEquals("3", $deck->getDeck()[40]->getRank());
+    }
+
+    /**
+     * Sort cards using sortCards() and verify that it is sorted.
+     * Use no arguments.
+     */
+    public function testShuffleCards()
+    {   
+        $deck = new Deck();
+        $deck->createDeck();
+        $deck->shuffleCards();
+        $rankString = strval($deck->getDeck()[0]->getRank()) . 
+                    strval($deck->getDeck()[1]->getRank()) . 
+                    strval($deck->getDeck()[2]->getRank()) . 
+                    strval($deck->getDeck()[3]->getRank()
+                );
+        $this->assertNotEquals("2345", $rankString);
+    }
+
+    /**
+     * Add jokers using addJokers() and verify that deck contains two jokers.
+     * Use no arguments.
+     */
+    public function testAddJokers()
+    {   
+        $deck = new Deck();
+        $deck->createDeck();
+        $deck->addJokers();
+        $this->assertEquals("Joker", $deck->getDeck()[52]->getRank());
+        $this->assertEquals("Joker", $deck->getDeck()[53]->getRank());
     }
 }
