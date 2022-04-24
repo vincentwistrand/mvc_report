f7a70b1e3adf1dd732dee272d77ad90ca37cf1bb
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-04-24 18:57:35 +0100

    Function used in CardAPIController to return json string

--- src/CardFunctions/cardsToJson.php
+++ src/CardFunctions/cardsToJson.php
@@ -9,11 +9,10 @@ namespace App\CardFunctions;
 function cardsToJson(array $cards): string 
 {
     $cardProperties = [];
-
     foreach ($cards as $card) {
         $cardProperties[] = $card->getProperties();
     }
-
+    
     $json_pretty = json_encode(json_decode(json_encode($cardProperties)), JSON_PRETTY_PRINT);
 
     return $json_pretty;
