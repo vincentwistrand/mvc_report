e66cee3aad48166d71bc4fe3f4163c634c84fec4
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-04-24 20:25:06 +0100

    Added method checkInput()

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -9,7 +9,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use App\CardFunctions\checkInput;
 
 class GameController extends AbstractController
 {
@@ -113,7 +112,7 @@ class GameController extends AbstractController
         $new_round  = $request->request->get('new_round');
         $reset  = $request->request->get('reset');
 
-        $result = \app\CardFunctions\checkInput($draw, $stop, $new_round, $reset, $session);
+        $result = $this->checkInput($draw, $stop, $new_round, $reset, $session);
 
         if ($result === "player") {
             $this->addFlash("info", "You Win!");
@@ -122,5 +121,78 @@ class GameController extends AbstractController
         }
 
         return $this->redirectToRoute('game-start');
+    }
+
+    /**
+    * Used in Class Game to see if there is a winner or if game should continue. 
+    * @return string
+    */
+    function checkInput(mixed $draw, mixed $stop, mixed $new_round, mixed $reset, SessionInterface $session): string
+    {
+        $game = $session->get("game") ?? 0;
+        $player_point = $session->get('player_points');
+        $bank_point = $session->get('bank_points');
+
+        if ($draw) {
+            $game->drawCardToPlayer();
+            $over21Points = $game->checkPlayerPoints();
+            if ($over21Points === true) {
+                $bank_point += 1;
+                $session->set('bank_points', $bank_point);
+                return "bank";
+            } else {
+                if ($game->getPlayerCardCount() >= 3) {
+                    $game->drawCardsToBank();
+
+                    $over21Points = $game->checkBankPoints();
+                    if ($over21Points === true) {
+                        $player_point += 1;
+                        $session->set('player_points', $player_point);
+                        return "player";
+                    }
+                    if ($game->playerWin() === true) {
+                        $player_point += 1;
+                        $session->set('player_points', $player_point);
+                        return "player";
+                    } else {
+                        $bank_point += 1;
+                        $session->set('bank_points', $bank_point);
+                        return "bank";
+                    }
+                }
+            }
+        } elseif ($stop) {
+            $game->drawCardsToBank();
+            $over21Points = $game->checkBankPoints();
+            if ($over21Points === true) {
+                $player_point += 1;
+                $session->set('player_points', $player_point);
+                return "player";
+            } else {
+                if ($game->playerWin() === true) {
+                    $player_point += 1;
+                    $session->set('player_points', $player_point);
+                    return "player";
+                } else {
+                    $bank_point += 1;
+                    $session->set('bank_points', $bank_point);
+                    return "bank";
+                }
+            }
+        } elseif ($new_round) {
+            $session->set('game', new \App\Card\Game());
+            $game = $session->get('game');
+            $game->newRound();
+        } elseif ($reset) {
+            $session->set('game', new \App\Card\Game());
+            $game = $session->get('game');
+            $game->newRound();
+
+            $player_point = 0;
+            $bank_point = 0;
+            $session->set('player_points', $player_point);
+            $session->set('bank_points', $bank_point);
+        }
+        return "continue";
     }
 }
