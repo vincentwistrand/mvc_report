4a765a8228feb964ac60c65d9ea7241c195543e7
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-04-25 13:36:47 +0100

    Added a test

--- tests/Card/PlayerTest.php
+++ tests/Card/PlayerTest.php
@@ -20,4 +20,14 @@ class PlayerTest extends TestCase
         $player = new Player("Player");
         $this->assertInstanceOf("\App\Card\Player", $player);
     }
+
+    /**
+     * Create new player object and verify that getPlayerId() return player id.
+     * Use no arguments.
+     */
+    public function testgetPlayerId()
+    {
+        $player = new Player("Hans");
+        $this->assertEquals("Hans", $player->getPlayerId());
+    }
 }
