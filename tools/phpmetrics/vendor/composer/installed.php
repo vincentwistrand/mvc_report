cdbe86e892e0d460d7f73ddea16db14c85b946dc
Vincent Wistrand <vincentwistrand@hotmail.se>
2022-05-06 10:35:59 +0100

    phpmetrics

---
+++ tools/phpmetrics/vendor/composer/installed.php
@@ -0,0 +1,53 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e6701043927ff5436881362152a5e51414df9cce',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e6701043927ff5436881362152a5e51414df9cce',
+            'dev_requirement' => false,
+        ),
+        'halleck45/php-metrics' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'halleck45/phpmetrics' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'nikic/php-parser' => array(
+            'pretty_version' => 'v4.13.2',
+            'version' => '4.13.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../nikic/php-parser',
+            'aliases' => array(),
+            'reference' => '210577fe3cf7badcc5814d99455df46564f3c077',
+            'dev_requirement' => false,
+        ),
+        'phpmetrics/phpmetrics' => array(
+            'pretty_version' => 'v2.8.1',
+            'version' => '2.8.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmetrics/phpmetrics',
+            'aliases' => array(),
+            'reference' => 'e279f7317390f642339941b693359e9a181817a7',
+            'dev_requirement' => false,
+        ),
+    ),
+);
